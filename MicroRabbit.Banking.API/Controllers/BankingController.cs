c1e4fc76050fdf3decf5523b88fb5db49c811349
Md-Atiqur-Rahman <atiqur.himel@selise.ch>
2025-02-09 01:57:20 +0600

    Implemented Send  Command and Event  Publish

--- MicroRabbit.Banking.API/Controllers/BankingController.cs
+++ MicroRabbit.Banking.API/Controllers/BankingController.cs
@@ -1,4 +1,5 @@
 using MicroRabbit.Banking.Application.Interfaces;
+using MicroRabbit.Banking.Application.Models;
 using MicroRabbit.Banking.Domain.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -22,6 +23,13 @@ namespace MicroRabbit.Banking.API.Controllers
         public ActionResult<IEnumerable<Account>> Get()
         {
             return Ok(_accountService.GetAccounts());
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] AccountTransfer accountTransfer)
+        {
+            _accountService.Transfer(accountTransfer);
+            return Ok(accountTransfer);
         }
 
     }
--- MicroRabbit.Infra.IoC/DependencyContainer.cs
+++ MicroRabbit.Infra.IoC/DependencyContainer.cs
@@ -1,7 +1,10 @@
-﻿using MicroRabbit.Banking.Application.Interfaces;
+﻿using MediatR;
+using MicroRabbit.Banking.Application.Interfaces;
 using MicroRabbit.Banking.Application.Services;
 using MicroRabbit.Banking.Data.Context;
 using MicroRabbit.Banking.Data.Repository;
+using MicroRabbit.Banking.Domain.CommandHandlers;
+using MicroRabbit.Banking.Domain.Commands;
 using MicroRabbit.Banking.Domain.Interfaces;
 using MicroRabbit.Domain.Core.Bus;
 using MicroRabbit.Infrastructure.Bus;
@@ -13,8 +16,15 @@ public static class DependencyContainer
 {
     public static void RegisterServices(IServiceCollection services)
     {
+
+        
+
         //Domain Bus
         services.AddTransient<IEventBus, RabbitmqBus>();
+
+        //Domain Banking Commands
+        services.AddTransient<IRequestHandler<CreateTransferCommand, bool>, TransferCommandHandler>();
+
 
         //Application Layer
         services.AddTransient<IAccountService, AccountService>();
