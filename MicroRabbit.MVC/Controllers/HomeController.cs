be003a3ff86675a73d84d1ad802e6c2cfd9bc947
Md-Atiqur-Rahman <atiqur.himel@selise.ch>
2025-02-10 01:20:12 +0600

    Presentation using MVC

---
+++ MicroRabbit.MVC/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using MicroRabbit.MVC.Models.DTO;
+using MicroRabbit.MVC.Models;
+using MicroRabbit.MVC.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MicroRabbit.MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private readonly ITransferService _transferService;
+
+        public HomeController(ITransferService transferService)
+        {
+            _transferService = transferService;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Transfer(TransferViewModel model)
+        {
+            TransferDto transferDto = new TransferDto()
+            {
+                FromAccount = model.FromAccount,
+                ToAccount = model.ToAccount,
+                TransferAmount = model.TransferAmount
+            };
+
+            await _transferService.Transfer(transferDto);
+
+            return View("Index");
+        }
+    }
+}
